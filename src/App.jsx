4867e46d79ec67998fec68d732ba8065f72311ae
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-07-15 19:57:50 +0100

    HW_78

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Card from './components/Card.jsx';
 
 function App(){
   return (
     <div className="App">
+      <Card title='Hello'/>
+      <Card text='some text V2'/>
+      <Card title='Hello' text='some text'/>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    const {title, text} = this.props;
+    return (
+      <div className="card">
+        <div className="card-body">
+          {title ? <h4 className="card-title">{title}</h4> : null}
+          {text ? <p className="card-text">{text}</p> : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
