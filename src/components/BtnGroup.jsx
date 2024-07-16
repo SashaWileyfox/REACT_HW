a4336279db8b95e14693d53c643aba5c41b6bbe7
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-07-16 19:03:26 +0100

    HW_83

--- src/components/BtnGroup.jsx
+++ src/components/BtnGroup.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import classNames from "classnames";
 
 class BtnGroup extends React.Component {
-  render() {
-    const btnClassLeft = classNames('btn btn-secondary left');
-    const btnClassRight = classNames('btn btn-secondary right');
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: '',
+    }
+  }
 
+  handleClick = (event) => {
+    const buttonName = event.target.textContent
+    this.setState({active: buttonName});
+    console.log("Натиснута кнопка:", buttonName);
+  }
+
+  render() {
+    const {active} = this.state;
     return (
       <div className="btn-group" role="group">
-        <button type="button" className={btnClassLeft}>Left</button>
-        <button type="button" className={btnClassRight}>Right</button>
+        <button type="button" className={classNames({'active': active === 'Left'}, 'btn btn-primary')}
+                onClick={this.handleClick}>Left
+        </button>
+        <button type="button" className={classNames({'active': active === 'Middle'}, 'btn btn-primary')}
+                onClick={this.handleClick}>Middle
+        </button>
+        <button type="button" className={classNames({ 'active': active === 'Right' }, 'btn btn-primary')}
+                onClick={this.handleClick}>Right</button>
       </div>
     )
   }
